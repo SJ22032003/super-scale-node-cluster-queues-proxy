2fc76b60375860098c554762452f9b9b2125f9b3
SJ22032003 <jaryalshaurya2203@gmail.com>
2025-02-01 13:45:27 +0530

    Refactor configuration files: update server name, script paths, and environment variables for clustering setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,33 +1,19 @@
 module.exports = {
-  apps : [{
-    name: "clustring server",
-    script: 'bun run dist/bundle.js',
-    watch: '.',
+  apps: [{
+    name: "clustering-server",
+    script: './dist/bundle.cjs',
+    watch: false,  // Change to false to prevent auto-reloading issues
     instances: 'MAX',
     autorestart: true,
-    max_memory_restart: '1G', // this means that if the memory usage of the process exceeds 1 GB, the process will be restarted.
+    max_memory_restart: '1G',
     exec_mode: 'cluster',
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'development',
+      PORT: 3001
     },
     env_production: {
-      NODE_ENV: 'production'
-    },
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      NODE_ENV: 'production',
+      PORT: 3001
     }
-  }
+  }]
 };
--- esbuild.config.js
+++ esbuild.config.js
@@ -4,8 +4,9 @@ import esbuild from 'esbuild';
 esbuild.build({
   entryPoints: ['index.ts'], // Entry point of your application
   bundle: true, // Bundle all dependencies
-  outfile: './dist/bundle.js', // Output file
+  outfile: './dist/bundle.cjs', // Output file
   platform: 'node', // Target platform (node for server-side, browser for client-side)
   minify: true, // Minify the output
   sourcemap: false, // Generate source maps
+  format: 'cjs', // Output format (cjs for Node.js)
 }).catch(() => process.exit(1));
